f8184f945290efdff973cccaea813e63536cfee0
Lunny Xiao <xiaolunwen@gmail.com>
2013-07-12 16:04:17 +0800

    init project

---
+++ play.go
@@ -0,0 +1,96 @@
+package play
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+type Result struct {
+	Errors string                   `json:"Errors"`
+	Events []map[string]interface{} `json:"Events"`
+}
+
+type FormatResult struct {
+	Body  string `json:"Body"`
+	Error string `json:"Error"`
+}
+
+func Format(codes string) (string, error) {
+	client := &http.Client{}
+	resp, err := client.PostForm("http://play.golang.org/fmt",
+		url.Values{"body": {codes}})
+	if err != nil {
+		return "", err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	if err != nil {
+		return "", err
+	}
+
+	res := new(FormatResult)
+	err = json.Unmarshal(body, res)
+	if err == nil {
+		if res.Error == "" {
+			return res.Body, nil
+		}
+		return "", errors.New(res.Error)
+	}
+
+	return "", err
+}
+
+func compileBytes(codes string) ([]byte, error) {
+	client := &http.Client{}
+	resp, err := client.PostForm("http://play.golang.org/compile",
+		url.Values{"version": {"2"}, "body": {codes}})
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+func CompileCode(codes string) (string, error) {
+	res, err := compileBytes(codes)
+	return string(res), err
+}
+
+func Compile(codes string) (*Result, error) {
+	res, err := compileBytes(codes)
+	if err != nil {
+		return nil, err
+	}
+
+	result := new(Result)
+	err = json.Unmarshal(res, result)
+	return result, err
+}
+
+func Share(codes string) (string, error) {
+	client := &http.Client{}
+	resp, err := client.PostForm("http://play.golang.org/share",
+		url.Values{"": {codes}})
+	if err != nil {
+		return "", err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("http://play.golang.org/p/%v", string(body)), nil
+}
---
+++ play_test.go
@@ -0,0 +1,62 @@
+package play
+
+import (
+	"fmt"
+	"testing"
+)
+
+var codes string = `
+	package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, playground")
+}
+	`
+
+func TestCompileCode(t *testing.T) {
+	res, err := CompileCode(codes)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	fmt.Println(res)
+}
+
+func TestFormat(t *testing.T) {
+	res, err := Format(codes)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	fmt.Println(res)
+}
+
+func TestCompile(t *testing.T) {
+	res, err := Compile(codes)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	fmt.Println(res)
+}
+
+func TestShare(t *testing.T) {
+	formatCodes, err := Format(codes)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	res, err := Share(formatCodes)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	fmt.Println(res)
+}
